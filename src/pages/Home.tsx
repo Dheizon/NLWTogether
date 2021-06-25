639b84e36a8b16de3f17bddc778e259a9ebf146f
Dheizon Gonçalves <dheizonx@gmail.com>
2021-06-25 12:22:10 -0300

    aula 3 Concluida

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -32,7 +32,7 @@ export function Home() {
         const roomRef = await database.ref(`rooms/${roomCode}`).get();
 
         if (!roomRef.exists()) {
-            alert('Room does not exists');
+            alert('Room does not exists.');
             return;
         }
         
@@ -54,7 +54,7 @@ export function Home() {
                         Crie sua sala com o google
                     </button>
                     <div className="separator">ou entre em uma sala</div>
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input
                             type="text" placeholder="Digite o codigo da sala" 
                             onChange={event => setRoomCode(event.target.value)}
